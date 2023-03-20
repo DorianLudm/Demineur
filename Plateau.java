940e31696b8011c30f42bcb027e896b3738fdea3
Plumatachi <114163684+Plumatachi@users.noreply.github.com>
2023-03-20 18:26:16 +0100

    Update files
    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plateau {
    private int nbLignes;
    private int nbColonnes;
@@ -6,12 +9,52 @@ public class Plateau {
    private List<CaseIntelligente> lePlateau;
 
    public Plateau(int nbLignes, int nbColonnes, int pourcentage){
-    this.nbLignes = nbLignes;
-    this.nbColonnes = nbColonnes;
-    this.pourcentageDeBombes = pourcentage;
+      this.nbLignes = nbLignes;
+      this.nbColonnes = nbColonnes;
+      this.pourcentageDeBombes = pourcentage;
+      this.lePlateau = new ArrayList<>();
    }
 
-   public void creerLesCasesVides(){
-    
+   private void creerLesCasesVides(){
+      int i = nbLignes;
+      int j = nbColonnes;
+      for (int k = 0; k < i*j-1; k++){
+         this.lePlateau.add(new CaseIntelligente());
+      }
+   }
+
+   private void rendLesCasesIntelligentes(){
+      for (CaseIntelligente c : this.lePlateau){
+         
+      }
+   }
+
+   protected void poseDesBombesAleatoirement(){}
+
+   public int getNbLignes(){return this.nbLignes;}
+   public int getNbColonnes(){return this.nbColonnes;}
+   public int getNbTotalBombes(){return this.nbBombes;}
+
+   public CaseIntelligente getCase(int numLigne, int numColonne){
+      for (int i = 0; i < nbLignes; i++){
+         for (int j = 0; j < nbColonnes; j++){
+            if (i == numLigne){
+               if (j == numColonne){
+                  return this.lePlateau.get(i).get(j);
+               }
+            }  
+         }
+      }
+      return null;
+   }
+
+   public int getNbCases(){return this.lePlateau.size();}
+
+   public void poseBombe(int x, int y){
+
+   }
+
+   public void reset(){
+      creerLesCasesVides();
    }
 }
