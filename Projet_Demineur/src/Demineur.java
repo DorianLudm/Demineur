cbac19ab35430fab72add6700e3bebf303f71f4b
Plumatachi <114163684+Plumatachi@users.noreply.github.com>
2023-03-30 09:17:09 +0200

    Update Demineur.java

--- Projet_Demineur/src/Demineur.java
+++ Projet_Demineur/src/Demineur.java
@@ -9,7 +9,54 @@ public class Demineur extends Plateau{
     }
 
     public int getScore(){return this.score;}
+
     public void reveler(int x, int y){
-        
+        int caseAReveler = x*super.getNbColonnes()+y;
+        super.lePlateau.get(caseAReveler).reveler();
+    }
+
+    public boolean marquer(int x, int y){
+        int caseAMarquer = x*super.getNbColonnes()+y;
+        super.lePlateau.get(caseAMarquer).marquer();
+    }
+
+    public boolean estGagnee(){
+        for(CaseIntelligente CaseElem: super.lePlateau){
+            if(CaseElem.contientUneBombe()){
+                if(!CaseElem.estMarquee()){
+                    return false;
+                }
+            }
+            else{
+                if(!CaseElem.estDecouverte()){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean estPerdu(){
+        for(CaseIntelligente CaseElem: super.lePlateau){
+            if(CaseElem.contientUneBombe()){
+                if(CaseElem.estDecouverte()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void reset(){
+        this.gameOver = false;
+        this.score = 0;
+    }
+
+    public void affiche(){
+
+    }
+
+    public void nouvellePartie(){
+
     }
 }
