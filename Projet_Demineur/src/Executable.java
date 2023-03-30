b3de84fc32fbff839359ae1c3d3991159f0071d6
Plumatachi <114163684+Plumatachi@users.noreply.github.com>
2023-03-30 08:58:23 +0200

    Update fonction rendLesCasesIntelligentes()

---
+++ Projet_Demineur/src/Executable.java
@@ -0,0 +1,7 @@
+public class Executable {
+    public static void main(String[] args) {
+        Plateau plateau = new Plateau(5, 4, 0);
+
+        System.out.println(plateau.creerLesCasesVides());
+    }
+}
--- Projet_Demineur/src/Plateau.java
+++ Projet_Demineur/src/Plateau.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Plateau {
    private int nbLignes;
@@ -24,12 +25,30 @@ public class Plateau {
    }
 
    private void rendLesCasesIntelligentes(){
-      for (CaseIntelligente c : this.lePlateau){
-         
+      for (int ligne = 0; ligne < nbLignes; ligne++){
+         for (int colonne = 0; colonne < nbColonnes; colonne++){
+            for (int ligneC = 0; ligneC < nbLignes; ligneC++){
+               for (int colonneC = 0; colonneC < nbColonnes; colonneC++){
+                  
+               }
+            }
+         }
       }
    }
 
-   protected void poseDesBombesAleatoirement(){}
+/**  */
+
+   protected void poseDesBombesAleatoirement(){
+        Random generateur = new Random();
+        for (int x = 0; x < this.getNbLignes(); x++){
+            for (int y = 0; y < this.getNbColonnes(); y++){
+                if (generateur.nextInt(100)+1 < this.pourcentageDeBombes){
+                    this.poseBombe(x, y);
+                    this.nbBombes = this.nbBombes + 1;
+                }
+            }
+        }
+    }
 
    public int getNbLignes(){return this.nbLignes;}
    public int getNbColonnes(){return this.nbColonnes;}
@@ -40,7 +59,7 @@ public class Plateau {
          for (int j = 0; j < nbColonnes; j++){
             if (i == numLigne){
                if (j == numColonne){
-                  return this.lePlateau.get(i).get(j);
+                  return this.lePlateau.get(i+j);
                }
             }  
          }
@@ -51,7 +70,7 @@ public class Plateau {
    public int getNbCases(){return this.lePlateau.size();}
 
    public void poseBombe(int x, int y){
-
+      
    }
 
    public void reset(){
